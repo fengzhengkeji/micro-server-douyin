4c4f5c57507df17478adc9333929ac35e67ac0ca
anoy <545544032@qq.com>
2018-11-07 17:07:40 +0800

    fix douyin api

--- src/main/java/com/anoyi/douyin/service/DouyinService.java
+++ src/main/java/com/anoyi/douyin/service/DouyinService.java
@@ -26,7 +26,7 @@ public class DouyinService {
 
     private final static String XMLHttpRequest = "XMLHttpRequest";
 
-    private final static String VIDEO_LIST_API = "https://www.iesdouyin.com/aweme/v1/aweme/post/?user_id=%s&count=21&max_cursor=%s&aid=1128&_signature=%s&dytk=%s";
+    private final static String VIDEO_LIST_API = "https://aweme.snssdk.com/aweme/v1/aweme/post/?iid=49056318644&device_id=49424741750&os_api=18&app_name=aweme&idfa=A0323AA0-7404-4C40-A197-F56435940521&device_platform=iphone&build_number=31006&vid=73FDACB2-B49A-403E-94DF-62B55CE64418&openudid=a46ca9b2600e4ca85c74c5eef29434a30e7b5ea9&device_type=iPhone7,1&app_version=3.1.0&js_sdk_version=1.3.0.1&version_code=3.1.0&os_version=12.1&screen_width=1242&aid=1128&pass-region=1&count=21&max_cursor=%s&user_id=%s&mas=010b172b781c26a10dbd49c6ba98f702ac5d1c01a7dff4cfea6f3e&as=a1059a3ea5d1eb15524585";
 
     private final static String USER_SHARE_API = "https://www.iesdouyin.com/share/user/%s";
 
@@ -38,11 +38,10 @@ public class DouyinService {
      * 获取抖音用户视频列表
      */
     public DyAweme videoList(String dyId, String dytk, String cursor) {
-        String signature = rpcNodeDyService.generateSignature(dyId);
-        String api = String.format(VIDEO_LIST_API, dyId, cursor, signature, dytk);
+        String api = String.format(VIDEO_LIST_API, cursor, dyId);
         String referer = String.format(REFERER_URL, dyId);
         try {
-            Document document = httpGetWithReffer(api, referer);
+            Document document = httpGetWithReferer(api, referer);
             return JSON.parseObject(document.text(), DyAweme.class);
         } catch (IOException e) {
             e.printStackTrace();
@@ -106,12 +105,10 @@ public class DouyinService {
     /**
      * HTTP 请求
      */
-    private Document httpGetWithReffer(String url, String referer) throws IOException {
+    private Document httpGetWithReferer(String url, String referer) throws IOException {
         Connection.Response response = Jsoup.connect(url)
                 .header("user-agent", UserAgent)
                 .header("x-requested-with", XMLHttpRequest)
-                .header("accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8")
-                .header("referer", referer)
                 .ignoreContentType(true).execute();
         String html = response.body().replace("&#xe", "");
         return Jsoup.parse(html);
